673f239d2c7a9f4d7900bb1fabfd2128a92cdb44
dipdastoko <dipdastoko@gmail.com>
2022-04-02 18:10:01 +0600

    ReviewItem added

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import useProducts from '../../hooks/useProducts';
 import useCart from '../../hooks/useCart'
+import Cart from '../Cart/Cart';
+import ReviewItem from '../ReviewItem/ReviewItem';
 
 const OrderReview = () => {
     const [products] = useProducts();
     const [cart] = useCart(products);
     return (
-        <div>
-            <h1>{products.length}</h1>
-            <h3>{useCart.length}</h3>
-            <h2>This is Order Review</h2>
+        <div className='shop-container'>
+            <div className='product-container'>
+                {
+                    cart.map(product => <ReviewItem product={product}></ReviewItem>)
+                }
+            </div>
+            <div className='class-container'>
+                <Cart cart={cart}></Cart>
+            </div>
+
         </div>
     );
 };
