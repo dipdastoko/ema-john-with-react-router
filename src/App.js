d58b988f953f3830ddf301c92bc70dac5ca83013
dipdastoko <dipdastoko@gmail.com>
2022-04-02 13:46:02 +0600

    Not Found Added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
+import Inventory from './components/Inventory/Inventory';
+import NotFound from './components/NotFound/NotFound';
+import OrderReview from './components/OrderReview/OrderReview';
 import Shop from './components/Shop/Shop';
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Shop></Shop>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Shop></Shop>}></Route>
+          <Route path='/shop' element={<Shop></Shop>}></Route>
+          <Route path='/review' element={<OrderReview></OrderReview>}></Route>
+          <Route path='/inventory' element={<Inventory></Inventory>}></Route>
+          <Route path='*' element={<NotFound></NotFound>}></Route>
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
